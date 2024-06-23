f73b1c3c9a3ba4ba3d30634851e50c33f787a694
RHUSHYA.K.C <123550676+Rhushya@users.noreply.github.com>
2024-06-23 18:01:37 +0530

    Add files via upload

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,21 @@
+
+import "./Nav.css";
+
+const Nav = ({ handleInputChange, query }) => {
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          className="search-input"
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          placeholder="Enter your search shoes."
+        />
+      </div>
+      
+    </nav>
+  );
+};
+
+export default Nav;
